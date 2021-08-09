116d5eccd1a8728fde86b588463c261be2cff16a
Akash-Patil-12 <patilakash121212@gmail.com>
2021-08-09 14:19:10 +0530

    [Akash] Add: Implemented show permutation of string using recursion

---
+++ Algorithm Programs/Permutation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm_Programs
+{
+    class Permutation
+    {
+        /// <summary>
+        /// Return permutation of string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="ans"></param>
+        public void PrintPermutation(string str, string ans)
+        {
+            if (str.Length == 0)
+            {
+                Console.WriteLine(ans + " ");
+                return;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                string ros = str.Substring(0, i) + str.Substring(i + 1);
+                PrintPermutation(ros, ans + ch);
+            }
+        }
+    }
+}
---
+++ Algorithm Programs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Algorithm_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation permutation = new Permutation();
+            permutation.PrintPermutation("abb", "");
+        }
+    }
+}
